6f7364c27a3f61aaf1242d6a2da2f7ac64b69d82
Khaled-Issa <khaled.ielmt@hotmail.com>
2019-11-30 20:20:12 +0200

    Added shorten Url feature

--- app.js
+++ app.js
@@ -5,15 +5,45 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const shortUrl = require('./models/shortUrl');
+
 app.use(bodyParser.json());
 app.use(cors());
 
+
+//connect to database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shortUrls')
+
+//allows node to find static content
 app.use(express.static(__dirname+'/public'));
 
 app.get('/new/:url(*)',function(req,res,next){
     var url = req.params.url;
 
-    return res.json({url});
+    //Regex for url
+    var regex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~$?&//=]*)?/gi;
+    
+    if(regex.test(url)== true){
+        var short= Math.floor(Math.random()*100000).toString();
+        var data = new shortUrl({
+            originalUrl: url,
+            shortUrl: short
+        });
+        
+        data.save(function(err){
+            if(err){
+                return res.send('Error saving to database');
+            }
+        });
+
+        return res.json({data});
+    }
+    var data = new shortUrl({
+        originalUrl: url,
+        shortUrl:'Invalid url'
+    });
+
+    return res.json(data);
 });
 
 
---
+++ models/shortUrl.js
@@ -0,0 +1,17 @@
+//structure of the shorURL document
+
+//require mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+
+    originalUrl: String,
+    shortUrl: String
+
+
+}); ({ timestamps: true});
+
+const ModelClass = mongoose.model('shortUrl', urlSchema);
+
+module.exports = ModelClass;
